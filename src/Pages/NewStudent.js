a83791beb1501fb316aa7009b70e848841d1da92
nandini3333 <nandinibhaskarreddy@gmail.com>
2023-02-01 23:36:10 +0530

    NewStudent Form

--- src/Pages/NewStudent.js
+++ src/Pages/NewStudent.js
@@ -1,38 +1,38 @@
 import "./NewStudent.css"
 
 const NewStudent = () => {
-    return(
+    return (
         <>
             <div id="form">
                 <form id="form-container" >
                     <div>
                         <label>Name
-                            <input type="text" placeholder="Type Name" name="name"  autoComplete="off" />
+                            <input type="text" placeholder="Type Name" name="name" autoComplete="off" />
                         </label>
                     </div>
                     <div>
                         <label>Age
-                            <input type="number" placeholder="Type Age" name="age"  autoComplete="off" />
+                            <input type="number" placeholder="Type Age" name="age" autoComplete="off" />
                         </label>
                     </div>
                     <div>
                         <label>Course
-                            <input type="text" placeholder="Type Course " name="course"  autoComplete="off" />
+                            <input type="text" placeholder="Type Course " name="course" autoComplete="off" />
                         </label>
                     </div>
                     <div>
                         <label>Batch
-                            <input type="text" placeholder="Type Batch " name="batch"  autoComplete="off" />
+                            <input type="text" placeholder="Type Batch " name="batch" autoComplete="off" />
                         </label>
                     </div>
 
-                    <button type="submit">Update</button>
-                    <button>Cancel</button>
                 </form>
-
+                <br/>
+                <button type="submit">Update</button>
+                <button>Cancel</button>
             </div>
-        
-    </>
+
+        </>
     )
 }
 
